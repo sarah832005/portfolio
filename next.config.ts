e541ba80e4f20cbf864266da3bd03ced9d9e0c89
sarahmahmoudi315 <mahmoudi.sarra@esprit.tn>
2026-05-13 15:52:46 +0200

    fix: add basePath and assetPrefix for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/portfolio",    // <--- Ajoute cette ligne
+  assetPrefix: "/portfolio", // <--- Ajoute cette ligne
   images: { unoptimized: true },
 };
 
